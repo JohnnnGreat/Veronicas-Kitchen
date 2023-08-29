cb9083c4deee0ee4320bd6966a68979c96b73072
John Ossai <johnossai20@gmai.com>
2023-08-29 12:14:00 -0700

    Added new sections

--- pages/our-story.js
+++ pages/our-story.js
@@ -15,27 +15,27 @@ function OurStory() {
       <div className="about">
         <div className="about__wrapper">
           <div className="text">
-            Veronica's Kitchen was born from a deep passion to delight the taste
-            buds of immigrants from Nigeria, West Africa, and beyond, by serving
-            authentic dishes that capture the essence of the motherland. As the
-            beloved flavors of Veronica's Kitchen spread, our clientele expanded
-            to embrace a diverse array of people from various demographics and
-            races. However, our unwavering mission remains unchanged - to
-            consistently deliver Nigerian and West African meals of the highest
-            quality that evoke nostalgia and create a culinary journey like no
-            other.
+            Veronica&apos;s Kitchen was born from a deep passion to delight the
+            taste buds of immigrants from Nigeria, West Africa, and beyond, by
+            serving authentic dishes that capture the essence of the motherland.
+            As the beloved flavors of Veronica&apos;s Kitchen spread, our
+            clientele expanded to embrace a diverse array of people from various
+            demographics and races. However, our unwavering mission remains
+            unchanged - to consistently deliver Nigerian and West African meals
+            of the highest quality that evoke nostalgia and create a culinary
+            journey like no other.
           </div>
           <div className="text">
-            From humble beginnings, Veronica's Kitchen has evolved into a haven
-            where cherished recipes are transformed into masterpieces. With each
-            dish, we aim to transport our guests to the heart of Nigeria,
-            offering a genuine taste of the rich cultural heritage and vibrant
-            culinary traditions. It is this dedication to authenticity that has
-            earned us the trust and loyalty of our patrons.
+            From humble beginnings, Veronica&apos;s Kitchen has evolved into a
+            haven where cherished recipes are transformed into masterpieces.
+            With each dish, we aim to transport our guests to the heart of
+            Nigeria, offering a genuine taste of the rich cultural heritage and
+            vibrant culinary traditions. It is this dedication to authenticity
+            that has earned us the trust and loyalty of our patrons.
           </div>
           <div className="text">
-            At Veronica's Kitchen, we believe that the soul of Nigerian and West
-            African cuisine lies in the meticulous selection of fresh,
+            At Veronica&apos;s Kitchen, we believe that the soul of Nigerian and
+            West African cuisine lies in the meticulous selection of fresh,
             high-quality ingredients, combined with time-honored cooking
             techniques passed down through generations. Our skilled chefs pour
             their passion and creativity into every dish, ensuring that flavors
