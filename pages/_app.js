ad47034bb920c47d26b61f2bc14682269eac9e8f
john ossai <johnossai20@gmail.com>
2023-10-13 00:13:50 -0700

    n

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "aos/dist/aos.css";
 import "@/styles/globals.scss";
 import "@/styles/layout.scss";
 import "@/styles/index.scss";
 import Header from "@/layout/Header";
 import Footer from "@/layout/Footer";
 import AOS from "aos";
-import "aos/dist/aos.css";
+
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
